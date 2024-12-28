b45fba6e1c803d09bd2a78bc63483dc4f44d16dd
Adnan Siddiqui <asr3012003@gmail.com>
2024-12-28 16:41:48 +0600

    Update AuthPage.tsx

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -24,7 +24,7 @@ export default function AuthPage() {
           <Image
             src="/onboarding/auth-model.png"
             alt=""
-            className="absolute -right-12 bottom-20 z-10 drop-shadow-[20px_0px_20px_rgba(0,0,0,0.15)]"
+            className="absolute -right-12 bottom-28 z-10 drop-shadow-[20px_0px_20px_rgba(0,0,0,0.15)]"
           />
           <Image
             src="/onboarding/bg-accent-2.svg"
@@ -33,9 +33,9 @@ export default function AuthPage() {
           />
         </div>
       )}
-      <div className="z-10 grid place-items-center absolute sm:relative  left-0 right-0 bottom-0  sm:bg-transparent  bg-gradient-to-t from-transparent to-foreground">
+      <div className="z-10 grid place-items-center absolute sm:relative  left-0 right-0 bottom-0 sm:rounded-b-3xl  sm:bg-transparent  bg-gradient-to-b from-transparent via-foreground to-foreground">
         {/* <div className="bg-gradient-to-b right-0 left-0 h-8 sm:hidden absolute sm:relative -top-8 to-foreground via-black/80 from-transparent " /> */}
-        <div className="sm:bg-foreground max-w-md py-6 pt-12 sm:p-6 sm:rounded-xl text-center">
+        <div className="sm:bg-foreground max-w-md  py-6 pt-64 sm:pt-6 sm:p-6 sm:rounded-3xl text-center">
           <h1 className="text-white text-xl font-medium">
             Automate content creation
           </h1>
