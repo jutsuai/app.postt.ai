de33d86f61a0308e20ca408c2df7bc69e84234cf
Saidul Badhon <31236650+SaidulBadhon@users.noreply.github.com>
2025-01-12 00:47:01 +0600

    Update ConnectLinkedinSuccessPage.tsx

--- src/pages/linkedin/connection/ConnectLinkedinSuccessPage.tsx
+++ src/pages/linkedin/connection/ConnectLinkedinSuccessPage.tsx
@@ -4,14 +4,31 @@ import httpClient from "@/lib/httpClient";
 import { useEffect, useState } from "react";
 import { RiExternalLinkLine } from "react-icons/ri";
 import { VscLoading } from "react-icons/vsc";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { toast } from "sonner";
-
 
 export default function ConnectLinkedinSuccessPage() {
   const [loading, setLoading] = useState(false);
+
+  const [loadingFetch, setLoadingFetch] = useState(false);
+
   const [organizationList, setOrganizationList] = useState<any>([]);
-  const navigate = useNavigate();
+
+  const addUserIfNotExists = (newUser: any) => {
+    setOrganizationList((prevUsers: any) => {
+      // Check if the user already exists
+      const exists = prevUsers.some(
+        (user: any) => user?.linkedinId === newUser?.linkedinId,
+      );
+      if (!exists) {
+        return [...prevUsers, newUser]; // Add the new user if not exists
+      }
+      console.log(
+        `User with linkedinId ${newUser?.linkedinId} already exists.`,
+      );
+      return prevUsers; // Return the previous array if the user exists
+    });
+  };
 
   useEffect(() => {
     handleGetOrganizationListFromDB();
@@ -23,14 +40,17 @@ export default function ConnectLinkedinSuccessPage() {
     httpClient()
       .get(`/linkedin/api/organizations`)
       .then((res) => {
-        console.log(res.data);
-        // setOrganizationList(res.data.data);
-        setOrganizationList( (curArray:any) =>  [...curArray, res.data.data]);
+        const data = res.data.data;
+        console.log(data);
 
-        if (res.data.data.length === 0) {
+        if (data.length === 0) {
           handleGetOrganizationListFromLinkedin();
 
           getUserDetails();
+        } else {
+          data?.map((org: any) => {
+            addUserIfNotExists(org);
+          });
         }
       })
       .catch((err) => {
@@ -43,47 +63,54 @@ export default function ConnectLinkedinSuccessPage() {
   };
 
   const handleGetOrganizationListFromLinkedin = () => {
-    setLoading(true);
+    setLoadingFetch(true);
 
     httpClient()
       .get(`/linkedin/api/organizations/sync`)
       .then((res) => {
         console.log(res.data);
 
-        setOrganizationList( (curArray:any) =>  [...curArray, res.data.data]);
+        // setOrganizationList((curArray: any) => [...curArray, res.data.data]);
+
+        res.data.data.forEach((org: any) => {
+          addUserIfNotExists(org);
+        });
       })
       .catch((err) => {
         console.error(err);
         toast.error(err?.response?.data?.message || "An error occurred");
       })
       .finally(() => {
-        setLoading(false);
+        setLoadingFetch(false);
       });
   };
 
   const getUserDetails = () => {
-    setLoading(true);
+    setLoadingFetch(true);
 
     httpClient()
       .get(`/linkedin/api/me`)
       .then((res) => {
         console.log(res.data);
         // setUserDetails(res.data.data);
-        setOrganizationList((curArray: any) => [...curArray, res.data.data]);
+        // setOrganizationList((curArray: any) => [...curArray, res.data.data]);
+
+        // check if this user is already in the arrya with linkedinId, if not add it
+        addUserIfNotExists(res.data.data);
       })
       .catch((err) => {
         console.error(err);
         toast.error(err?.response?.data?.message || "An error occurred");
       })
       .finally(() => {
-        setLoading(false);
+        setLoadingFetch(false);
       });
   };
 
   return (
-    <div className="lg:max-w-md w-full h-full flex flex-col gap-2 px-4 items-start  pt-[7dvh]">
-      <div className=" flex w-full justify-start flex-col items-center  h-full mt-9 gap-4">
-        <div className="flex items-center justify-center  relative">
+    <div className="flex h-full w-full flex-col items-start gap-2 px-4 pt-[7dvh] lg:max-w-md">
+      <div className="mt-9 flex h-full w-full flex-col items-center justify-start gap-4">
+        <div className="relative flex items-center justify-center">
           <img
             src="/onboarding/social-linkedin.svg"
             alt=""
@@ -109,25 +136,25 @@ export default function ConnectLinkedinSuccessPage() {
           </Button> */}
         </div>
 
-        <h3 className="text-lg font-semibold text-center ">
+        <h3 className="text-center text-lg font-semibold">
           LinkedIn Connected
         </h3>
 
-        <p className="text-muted-foreground text-sm -mt-2 text-center">
+        <p className="-mt-2 text-center text-sm text-muted-foreground">
           Your LinkedIn profile has been successfully connected
         </p>
 
-        <div className="flex flex-col gap-2 max-h-[40dvh] px-4 overflow-y-auto">
+        <div className="flex max-h-[40dvh] flex-col gap-2 overflow-y-auto px-4">
           {organizationList?.map((org: any, index: any) => (
             <div
               key={index}
-              className="flex gap-3 bg-primary/10 p-2 rounded-full items-center "
+              className="flex items-center gap-3 rounded-full bg-primary/10 p-2"
             >
               <BoringAvatar name={org.name} size={48} className="min-w-12" />
 
               <div className="flex flex-col">
                 <h3 className="text-sm font-semibold">{org.name}</h3>
-                <p className="text-muted-foreground text-xs  line-clamp-1">
+                <p className="line-clamp-1 text-xs text-muted-foreground">
                   {org.description}
                 </p>
               </div>
@@ -136,7 +163,7 @@ export default function ConnectLinkedinSuccessPage() {
                 href={org.linkedinUrl}
                 target="_blank"
                 rel="noreferrer"
-                className="text-blue-500 pr-5 underline"
+                className="pr-5 text-blue-500 underline"
               >
                 <RiExternalLinkLine />
               </a>
@@ -144,10 +171,25 @@ export default function ConnectLinkedinSuccessPage() {
           ))}
         </div>
 
-        <Button className="w-full rounded-full mt-auto min-h-[56px]" asChild>
+        <Button className="mt-auto min-h-[56px] w-full rounded-full" asChild>
           <Link className="w-full" to="/">
             {loading ? <VscLoading className="animate-spin" /> : "Let's Go!"}
           </Link>
+        </Button>
+
+        <Button
+          variant="ghost"
+          className="w-full rounded-full"
+          onClick={() => {
+            handleGetOrganizationListFromLinkedin();
+            getUserDetails();
+          }}
+        >
+          {loadingFetch ? (
+            <VscLoading className="animate-spin" />
+          ) : (
+            "Fetch again"
+          )}
         </Button>
       </div>
     </div>
